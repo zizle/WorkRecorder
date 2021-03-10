1c58919311d0eb8b26c4f2adb0e66c7164946636
zizle <462894999@qq.com>
2021-03-10 16:55:30 +0800

    2.1.0219

--- src/view/download/download-file.vue
+++ src/view/download/download-file.vue
@@ -2,8 +2,8 @@
 <div>
   <div class="download">
     <p style="font-size:28px">分析决策系统(研究员)下载</p>
-    <p style="font-size:18px">最新版本: <a href="http://210.13.218.130:9004/static/INSTALLER/INSIDE/FAClientInsideSetup_1.5.3.exe">Windows-7 Windows-10 Version 1.5.3</a></p>
-    <p style="font-size:12px">发布日期: 2021年02月01日</p>
+    <p style="font-size:18px">最新版本: <a href="http://210.13.218.130:9004/static/INSTALLER/INSIDE/FAClientInsideSetup_1.5.7.exe">Windows-7 Windows-10 Version-1.5.7</a></p>
+    <p style="font-size:12px">发布日期: 2021年03月10日</p>
     <p>特别说明:<p>
     <p>由于权限的管理,<span style="color:rgb(210,55,55)">首次</span>安装注册用户后,请进入菜单<span>"关于系统"</span> - <span>"版本检查"</span>将<span>客户端号</span>提供给管理员开通登录.</p>
     <p style="color:rgb(233,66,66);font-weight:bold">--安装时若有360等软件拦截请允许通过,否则可能无法使用！--</p>
